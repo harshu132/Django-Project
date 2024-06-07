0c1f9b5a7ff128108ff905343717d9a3be470c45
Rupesh Vishwakarma <vrupesh132@gmail.com>
2024-06-07 17:36:17 +0530

    i change in Login authentication

--- ecapp/urls.py
+++ ecapp/urls.py
@@ -1,10 +1,10 @@
-
 from django.urls import path
+from .views import CustomLogoutView
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordResetForm
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm,MySetPasswordForm
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -16,10 +16,15 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
 
-    #login authentication 
-    path('registration/',views.CustomerRegistrationView.as_view(),name='customerregistration'),
-    path('accounts/login/', auth_view.LoginView.as_view(template_name='ecapp/login.html',authentication_form=LoginForm),name='login'),
-    path('password-reset/',auth_view.PasswordResetView.as_view(template_name="ecapp/password_reset.html",form_class=MyPasswordResetForm),
-         name='password_reset')
-    
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Login authentication 
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='ecapp/login.html', authentication_form=LoginForm), name='login'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='ecapp/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
+    path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='ecapp/passwordchangedone.html'), name='passwordchangedone'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+
+    path('password-reset',auth_views.PasswordResetView.as_view(template_name='ecapp/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='ecapp/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='ecapp/password_reset_confirm.html',form_class=MySetPasswordForm),name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='ecapp/password_reset_complete.html'),name='password_reset_complete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecapp/views.py
+++ ecapp/views.py
@@ -1,63 +1,16 @@
 from django.db.models import Count
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 from django.views import View
-from . models import Product,Customer
-from . forms import CustomerProfileForm, CustomerRegistrationForm
+from .models import Product, Customer
+from .forms import CustomerProfileForm, CustomerRegistrationForm
 from django.contrib import messages
 
-from django.views.generic import TemplateView
-
-class ProfileView(TemplateView):
-    template_name = 'ecapp/profile.html'
-
-def home(request):
-    # Logic for the home view
-    return render(request, 'ecapp/home.html')
-
-def about(request):
-    # Logic for the home view
-    return render(request, 'ecapp/about.html')
-
-def contact(request):
-    # Logic for the home view
-    return render(request, 'ecapp/contact.html')
-
-class CategoryView(View):
-    def get(self, request,val):
-       product = Product.objects.filter(category=val)
-       title = Product.objects.filter(category=val).values('title')
-       return render(request, 'ecapp/category.html',locals())
-    
-class CategoryTitle(View):
-    def get(self, request,val):
-       product = Product.objects.filter(title=val)
-       title = Product.objects.filter(category=product[0].category).values('title')
-       return render(request, 'ecapp/category.html',locals())    
-
-class ProductDetail(View):
-    def get(self,request,pk):
-        product = Product.objects.get(pk=pk)
-        return render(request,"ecapp/productdetail.html",locals())
-
-
-class CustomerRegistrationView(View):
-    def get(self,request):
-        form = CustomerRegistrationForm()
-        return render(request, 'ecapp/customerregistrstion.html',locals())
-    def post(self,request):
-        form = CustomerRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request,"Congratulations! User Register Successfully")
-        else:
-            messages.error(request,"Invalid Input Data")
-        return render(request,'ecapp/customerregistrstion.html',locals())  
-
 class ProfileView(View):
-    def get(self,request):
+    def get(self, request):
         form = CustomerProfileForm()
-        return render(request, 'ecapp/profile.html',locals())
-    def post(self,request):
+        return render(request, 'ecapp/profile.html', locals())
+    def post(self, request):
         form = CustomerProfileForm(request.POST)
         if form.is_valid():
             user = request.user
@@ -68,13 +21,57 @@ class ProfileView(View):
             state = form.cleaned_data['state']
             zipcode = form.cleaned_data['zipcode']
 
-            reg = Customer(user=user,name=name,locality=locality,mobile=mobile,city=city,state=state,zipcode=zipcode)
+            reg = Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, state=state, zipcode=zipcode)
             reg.save()
-            messages.success(request,"Congratulations!  Your Profile Save Successfully")
+            messages.success(request, "Congratulations! Your Profile Save Successfully")
         else:
-            messages.warning(request,"Invalid Input Data")    
-        return render(request, 'ecapp/profile.html',locals())          
-    
+            messages.warning(request, "Invalid Input Data")
+        return render(request, 'ecapp/profile.html', locals())
+
+def home(request):
+    return render(request, 'ecapp/home.html')
+
+def about(request):
+    return render(request, 'ecapp/about.html')
+
+def contact(request):
+    return render(request, 'ecapp/contact.html')
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, 'ecapp/category.html', locals())
+
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, 'ecapp/category.html', locals())
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "ecapp/productdetail.html", locals())
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'ecapp/customerregistration.html', locals())
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Congratulations! User Register Successfully")
+        else:
+            messages.error(request, "Invalid Input Data")
+        return render(request, 'ecapp/customerregistration.html', locals())
+
 def address(request):
     add = Customer.objects.filter(user=request.user)
-    return render(request, 'ecapp/address.html',locals())    
+    return render(request, 'ecapp/address.html', locals())
+
+class CustomLogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect("home")
